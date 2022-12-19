06a88e71a11f3669dc3964bf452062afef8b823e
Coding Fish <110103843+ghlrobin@users.noreply.github.com>
2022-12-19 15:25:59 +0900

    Update dot_ubuntu.sh

--- dot_ubuntu.sh
+++ dot_ubuntu.sh
@@ -5,9 +5,9 @@ sudo apt-get update
 # sudo apt-get -y upgrade
 sudo apt-get install -y vim curl awscli python3-pip tmux fonts-powerline git-lfs tree
 sudo apt -y autoremove
-pip3 install -U pip && pip3 install -U awscli  # For fixing awscli issue
-pip3 install nvitop
-pip3 install gpustat
+sudo apt-get install htop
+pip install -U pip && pip install -U awscli  # For fixing awscli issue
+pip install gpustat
 # Install zsh
 sudo apt-get install -y zsh
 sudo chsh -s /bin/zsh "$USER"

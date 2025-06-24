17592bc074fcc821491b3d1196ff21961b375ced
Ganghyeok Lee (Robin) <110103843+ghlrobin@users.noreply.github.com>
2025-06-24 10:02:53 +0900

    Update dot_ubuntu.sh

--- dot_ubuntu.sh
+++ dot_ubuntu.sh
@@ -4,7 +4,7 @@
 apt-get update
 apt-get install sudo
 # sudo apt-get -y upgrade
-sudo apt-get install -y vim curl awscli python3-pip tmux fonts-powerline git-lfs tree htop gpustat
+sudo apt-get install -y vim curl awscli python3-pip tmux fonts-powerline git-lfs tree htop gpustat wget
 sudo apt -y autoremove
 pip install -U pip && pip install -U awscli  # For fixing awscli issue
 # Install zsh
